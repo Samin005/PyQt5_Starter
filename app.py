1d18428791cbbd9d61d206156c0a1f78d2b51f96
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2021-02-27 01:11:22 +0600

    Dynamically creates tree based on hard-coded level.

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from gui.main import Ui_Dialog
 
 
-def greet(user_name):
-    global ui
-    print("Hello", user_name)
-    ui.label.setText("Hello, " + user_name)
+def create_tree(node_name):
+    global ui, row_no, column_no
+    print("Creating tree for: ", node_name)
+    level = 4
+    for x in range(level):
+        column_no = 0
+        for y in range(2**x):
+            ui.label = QtWidgets.QLabel(ui.frame_tree)
+            ui.label.setObjectName("userLabel_" + str(row_no) + str(column_no))
+            column_span = 2**(level-1)//(2**x)
+            ui.gridLayout_4.addWidget(ui.label, row_no, column_no * column_span, 1, column_span,
+                                      alignment=QtCore.Qt.AlignCenter)
+            ui.label.setText("Node name: " + node_name)
+            column_no += 1
+        row_no += 1
 
 
 app = QtWidgets.QApplication(sys.argv)
@@ -14,10 +25,9 @@ Dialog = QtWidgets.QDialog()
 ui = Ui_Dialog()
 ui.setupUi(Dialog)
 
-ui.label = QtWidgets.QLabel(ui.layoutWidget)
-ui.label.setObjectName("userLabel")
-ui.gridLayout.addWidget(ui.label, 1, 0, 1, 3)
-ui.pushButton.clicked.connect(lambda: greet(ui.lineEdit.text()))
+row_no = 0
+column_no = 0
+ui.button_create_tree.clicked.connect(lambda: create_tree(ui.lineEdit_nodeName.text()))
 
 
 Dialog.show()
