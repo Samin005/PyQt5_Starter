7fb6b0fd731852a3cdff52691dfa53810bff8b68
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2021-02-27 17:32:00 +0600

    Added style to tree

--- app.py
+++ app.py
@@ -6,13 +6,19 @@ from gui.main import Ui_Dialog
 def create_tree(node_name):
     global ui, row_no, column_no
     print("Creating tree: ", node_name)
-    level = 6
+    level = 4
     branching_factor = 2
     for x in range(level):
         column_no = 0
         for y in range(branching_factor**x):
             ui.label = QtWidgets.QLabel(ui.frame_tree)
             ui.label.setObjectName("userLabel_" + str(row_no) + str(column_no))
+            ui.label.setStyleSheet("background-color: white;\n"
+                                   "border: 5px solid black;\n"
+                                   "border-radius: 30%;\n"
+                                   "font-size: 45px;\n"
+                                   "padding: 5%;")
+            ui.label.setAlignment(QtCore.Qt.AlignCenter)
             column_span = branching_factor**(level-1)//(branching_factor**x)
             ui.gridLayout_4.addWidget(ui.label, row_no, column_no * column_span, 1, column_span,
                                       alignment=QtCore.Qt.AlignCenter)
